956804a82d31c8509dec2ad85694116a2050f8e4
Mazen Thabet <MazenThabet2@outlook.com>
2026-05-19 21:21:17 +0300

    Add project files.

---
+++ G-NET-26-Linq-01/Program.cs
@@ -0,0 +1,17 @@
+﻿#region Using directives
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+#endregion
+namespace G_NET_26_Linq_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
