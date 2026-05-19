dbcb3accbc80ed515c987717d9fd89941ef6dd45
Mazen Thabet <MazenThabet2@outlook.com>
2026-05-19 21:31:11 +0300

    Question 1

--- G-NET-26-Linq-01/Program.cs
+++ G-NET-26-Linq-01/Program.cs
@@ -10,8 +10,30 @@ namespace G_NET_26_Linq_01
 {
     public class Program
     {
+        public class Product
+        {
+            public string Name { get; set; }
+            public string Category { get; set; }
+        }
         public static void Main(string[] args)
         {
+            #region Q1
+                //List<Product> products = new List<Product>
+                //{
+                //new Product { Name = "Ikura", Category = "Seafood" },
+                //new Product { Name = "Chai", Category = "Beverages" },
+                //new Product { Name = "Boston Crab Meat", Category = "Seafood" },
+                //new Product { Name = "Tofu", Category = "Produce" }
+                //};
+
+                //var seafoodProducts = products.Where(p => p.Category == "Seafood");
+
+                //foreach (var product in seafoodProducts)
+                //{
+                //    Console.WriteLine($"Name: {product.Name},Category: {product.Category}");
+                //}
+
+            #endregion
         }
     }
 }
